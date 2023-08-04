f5d27aa4a35be1767084e935b6cff7b40cb56e34
mesropt <mesroptnamak@gmail.com>
2023-08-04 18:01:00 +0400

    Develop_m6. Small changes in api.py

--- flask_blog/api.py
+++ flask_blog/api.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_blog.models import Post
 from flask_blog import create_app
+from flask_cors import CORS
 
 app = create_app()
 api = Api(app)
+CORS(app)  # Enable CORS for your app
 
 class AllPostsResource(Resource):
     def get(self):
@@ -70,4 +73,4 @@ api.add_resource(AllPostsResource, '/api/posts')
 api.add_resource(SinglePostResource, '/api/posts/<int:post_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))  # Use the Heroku-assigned port
